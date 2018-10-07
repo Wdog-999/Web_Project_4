a4d2c109b2009b5544b649428d29aa9542942d57
Wdog-999 <wdog_999@yahoo.com>
2018-10-06 23:15:58 -0400

    Progress

--- Lab4/Controllers/Lab4Controller.cs
+++ Lab4/Controllers/Lab4Controller.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WaffleGenerator;
 
 namespace Lab4.Controllers
 {
@@ -10,6 +8,23 @@ namespace Lab4.Controllers
     {
         public IActionResult Index()
         {
+            DateTime date = DateTime.Now;
+            DateTime noon = new DateTime(date.Year, date.Month, date.Day, 12, 0, 0);
+            DateTime evening = new DateTime(date.Year, date.Month, date.Day, 18, 0, 0);
+            DateTime nextYear = new DateTime(date.Year + 1, 1, 1, 0, 0, 0);
+            TimeSpan time = nextYear - date;
+            ViewData["time"] = date.ToShortTimeString();
+            ViewData["date"] = date.ToLongDateString();
+            ViewData["year"] = date.Year;
+            ViewData["hour"] = date.Hour;
+            ViewData["days"] = time.Days;
+            return View();
+        }
+
+        public IActionResult Waffles()
+        {
+            var html = WaffleEngine.Html(paragraphs: 2, includeHeading: true, includeHeadAndBody: false);
+            ViewData["waffle"] = html;
             return View();
         }
     }
